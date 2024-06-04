ea562a1c410fe2c5474eae4ca1c539e94fcb5f29
Rick4578 <100131988+Rick4578@users.noreply.github.com>
2024-06-05 01:06:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,119 +1,206 @@
 
-//reset function bring everything to default value
+function onUserPress()
+{
+  let randomComp = Math.floor(Math.random() * 3);
+
+  
+  if (randomComp==0)
+    { 
+      document.getElementById("display_demo").style.display="none"
+      document.getElementById("comp_imgChange").style.display="none"
+      document.getElementById("comp_imgChangestatic").style.display= "none" ;
+      document.getElementById("comp_imgChangerotate").style.display= "block" ;
+      setTimeout(function() 
+      { 
+        document.getElementById("comp_imgChange").src="assects/R.png";
+        document.getElementById("comp_imgChange").style.display="block"
+        document.getElementById("comp_imgChangerotate").style.display= "none"
+        document.getElementById("display_demo").innerHTML=randomComp
+        document.getElementById("display_demo").style.display="block"
+
+      },1000);
+      randomComp="Stone"
+    } 
+  
+  if (randomComp==1)
+    { 
+      document.getElementById("display_demo").style.display="none"
+      document.getElementById("comp_imgChange").style.display="none"
+      document.getElementById("comp_imgChangestatic").style.display= "none" ;
+      document.getElementById("comp_imgChangerotate").style.display= "block" ;
+      setTimeout(function() 
+      { 
+        document.getElementById("comp_imgChange").src="assects/P.png";
+        document.getElementById("comp_imgChange").style.display="block"
+        document.getElementById("comp_imgChangerotate").style.display= "none"
+        document.getElementById("display_demo").innerHTML=randomComp
+        document.getElementById("display_demo").style.display="block"
+
+      },1000);
+      randomComp="Paper"
+      
+      
+    }
+
+  if (randomComp==2)
+    { 
+      document.getElementById("display_demo").style.display="none"
+      document.getElementById("comp_imgChange").style.display="none"
+      document.getElementById("comp_imgChangestatic").style.display= "none" ;
+      document.getElementById("comp_imgChangerotate").style.display= "block" ;
+      setTimeout(function() 
+      { 
+        document.getElementById("comp_imgChange").src="assects/S.png";
+        document.getElementById("comp_imgChange").style.display="block"
+        document.getElementById("comp_imgChangerotate").style.display= "none"
+        document.getElementById("display_demo").innerHTML=randomComp
+        document.getElementById("display_demo").style.display="block"
+      },1000);
+      randomComp="Scissor"
+
+      
+    
+    
+    }
+  
+  document.getElementById("demo").innerHTML=randomComp;
+
+  let userInput=document.getElementById("userData").innerHTML
+  
+  if(randomComp==userInput)
+    {
+      document.getElementById("winner").innerHTML="It Is a Draw"
+      setTimeout(function(){document.getElementById("display_winner").innerHTML="It Is a Draw"},2000)
+    }
+  else if(userInput=="Stone" && randomComp=="Scissor")
+    {
+      document.getElementById("winner").innerHTML="The Winner is User"
+      setTimeout(function(){document.getElementById("display_winner").innerHTML="The Winner is User"},2000)
+    }
+  else if(userInput=="Paper" && randomComp=="Stone")
+    {
+      document.getElementById("winner").innerHTML="The Winner is User"
+      setTimeout(function(){document.getElementById("display_winner").innerHTML="The Winner is User"},2000)
+    }
+  else if(userInput=="Scissor" && randomComp=="Paper")
+    {
+      document.getElementById("winner").innerHTML="The Winner is User"
+      setTimeout(function(){document.getElementById("display_winner").innerHTML="The Winner is User"},2000)
+    }
+  else
+    {
+      document.getElementById("winner").innerHTML="The Winner is Computer"
+      setTimeout(function(){document.getElementById("display_winner").innerHTML="The Winner is Computer"},2000)
+    }
+}
+   
 
 
-function re()
-{
-document.getElementById("demo").innerHTML="--------------";
-document.getElementById("userData").innerHTML="please press ArrowUp for stone ArrowLeft for paper and ArrowRight for scissors";
-document.getElementById("computercount").innerHTML= 0;
-document.getElementById("usercount").innerHTML= 0;
-document.getElementById("winner").innerHTML= "--------------";
+function Stone()
+{ 
+  document.getElementById("display_winner").innerHTML="--------------"
+  document.getElementById("display_userData").style.display="none"
+  document.getElementById("user_imgChange").style.display="none"
+  document.getElementById("user_imgChangestatic").style.display= "none" ;
+  document.getElementById("user_imgChangerotate").style.display= "block" ;
+  setTimeout(function() 
+  {document.getElementById("user_imgChange").src="assects/R.png";
+   document.getElementById("user_imgChange").style.display="block"
+   document.getElementById("user_imgChangerotate").style.display= "none"
+  },2300);
+  document.getElementById("userData").innerHTML="Stone"
+  setTimeout(function() 
+  {
+    document.getElementById("display_userData").innerHTML="Stone"
+    document.getElementById("display_userData").style.display="block"                 
+  },2500) ;
+  
+  onUserPress();  
+  click();
+}
+
+function paper()
+{  
+  document.getElementById("display_winner").innerHTML="--------------"
+  document.getElementById("display_userData").style.display="none"
+  document.getElementById("user_imgChange").style.display="none"
+  document.getElementById("user_imgChangestatic").style.display= "none" ;
+  document.getElementById("user_imgChangerotate").style.display= "block" ;
+  setTimeout(function() 
+  {document.getElementById("user_imgChange").src="assects/P.png";
+   document.getElementById("user_imgChange").style.display="block"
+   document.getElementById("user_imgChangerotate").style.display= "none"
+  },2300);
+  document.getElementById("userData").innerHTML="Paper"
+  setTimeout(function() 
+  {
+    document.getElementById("display_userData").innerHTML="Paper"
+    document.getElementById("display_userData").style.display="block"
+  
+  },2500) ;
+    
+  onUserPress(); 
+  click();  
+}
+
+function scissor()
+{ document.getElementById("display_winner").innerHTML="--------------"
+  document.getElementById("display_userData").style.display="none"
+  document.getElementById("user_imgChange").style.display="none"
+  document.getElementById("user_imgChangestatic").style.display= "none" ;
+  document.getElementById("user_imgChangerotate").style.display= "block" ;
+  setTimeout(function() 
+  {document.getElementById("user_imgChange").src="assects/S.png";
+   document.getElementById("user_imgChange").style.display="block"
+   document.getElementById("user_imgChangerotate").style.display= "none"
+  },2300);
+  document.getElementById("userData").innerHTML="Scissor"
+  setTimeout(function() 
+  {
+    document.getElementById("display_userData").innerHTML="Scissor"
+    document.getElementById("display_userData").style.display="block"
+  
+  },2500) ;
+  
+  onUserPress(); 
+  click();   
 }
 
 
-//the main working function taht workes when an arrow key is pressed as an input 
-//it generate a random value for computer among stone apaper and scissors
-//and compare it with the value of user input and winner is decided 
-
-function onKeyPress()
-{
-let key = event.key;
-   let k= Math.floor(Math.random() * 3);
-   if (k==0)
-   {
-    k="stone"
-   }
-   else if(k==1)
-   {
-    k="paper" 
-   } 
-   else {
-   k="scissors"
-   }
-  document.getElementById("demo").innerHTML = k ;
 
 
-if (key=="ArrowUp")
-{
-document.getElementById("demo").innerHTML = k ;
-document.getElementById("userData").innerHTML ="stone"
 
-}
-else if (key=="ArrowLeft")
-{
- document.getElementById("demo").innerHTML = k ;
- document.getElementById("userData").innerHTML ="paper"
-}
-else if(key=="ArrowRight")
-{
-document.getElementById("demo").innerHTML = k ;
-document.getElementById("userData").innerHTML ="scissors"
-}
-else 
-{
-document.getElementById("userData").innerHTML ="please press ArrowUp for stone ArrowLeft for paper and ArrowRight for scissors "
-}
-{
- 
-}
-let dataColl=document.getElementById("userData").innerHTML
-if (dataColl==k)
-{
-document.getElementById("winner").innerHTML="it's a draw"
-}
-else if (dataColl=="stone"& k=="scissors")
-{
-document.getElementById("terms").innerHTML="user own"
-}
-else if (dataColl=="paper"& k=="stone")
-{
-document.getElementById("winner").innerHTML="user own"
-}
-else if (dataColl=="scissors"& k=="paper")
-{
-document.getElementById("winner").innerHTML="user own"
-}
-else
-{
-document.getElementById("winner").innerHTML="Computer own"
-}
-}
-
-
-//var is used beacause variable is declared outside the function and used inside the function to make it use as a loop 
-//it counts computer and user winning count and gives the result after 5 matches
-
-var datacounter=[ ];
-var clickcount=0;
 var compcounter=0;
 var usercounter=0;
 function click()
 {
-clickcount=clickcount+1
-if (document.getElementById("winner").innerHTML=="user own")
+
+if (document.getElementById("winner").innerHTML=="The Winner is User")
 {
 usercounter=usercounter+1
-document.getElementById("usercount").innerHTML=usercounter;
+setTimeout(function(){document.getElementById("usercount").innerHTML=usercounter;},2000)
+
 }
-else if(document.getElementById("winner").innerHTML=="Computer own")
+else if(document.getElementById("winner").innerHTML=="The Winner is Computer")
 {
 compcounter=compcounter+1
-document.getElementById("computercount").innerHTML=compcounter;
+setTimeout (function(){document.getElementById("computercount").innerHTML=compcounter;},2000)
+
 }
 if(usercounter+compcounter==5)
 { 
  if(usercounter>compcounter)
   {
    document.getElementById("term").innerHTML="after 5 matches the winner is User";
-   re();
+   half_re();
    usercounter=0;
    compcounter=0;
   }
  else
   {
     document.getElementById("term").innerHTML="after 5 matches the winner is Computer";
-    re();
+   
+    half_re();
     usercounter=0;
     compcounter=0;
   }
@@ -121,13 +208,54 @@ if(usercounter+compcounter==5)
 
 }
 
-
-//rather then calling multiple function when on click they are bundle together 
-
-function mo()
+function re()
 {
-onKeyPress()
-click()
+document.getElementById("demo").innerHTML="--------------";
+document.getElementById("userData").innerHTML="please choose an input";
+document.getElementById("computercount").innerHTML= 0;
+document.getElementById("usercount").innerHTML= 0;
+document.getElementById("winner").innerHTML= "--------------";
+document.getElementById("display_winner").innerHTML= "--------------";
+usercounter=0;
+compcounter=0;
+document.getElementById("user_imgChangestatic").style.display= "block" ;
+document.getElementById("user_imgChangerotate").style.display= "none" ;
+document.getElementById("user_imgChange").src="";
+document.getElementById("user_imgChange").style.display="none";
+
+document.getElementById("comp_imgChangestatic").style.display= "block" ;
+document.getElementById("comp_imgChangerotate").style.display= "none" ;
+document.getElementById("comp_imgChange").src="";
+document.getElementById("comp_imgChange").style.display="none";
+
+
+
+document.getElementById("display_demo").style.display="none"
+document.getElementById("display_userData").style.display="none"
 }
 
 
+function  half_re()
+{
+document.getElementById("demo").innerHTML="--------------";
+document.getElementById("userData").innerHTML="please choose an input";
+document.getElementById("computercount").innerHTML= 0;
+document.getElementById("usercount").innerHTML= 0;
+document.getElementById("winner").innerHTML= "--------------";
+document.getElementById("display_winner").innerHTML= "--------------";
+usercounter=0;
+compcounter=0;
+
+document.getElementById("user_imgChangerotate").style.display= "none" ;
+document.getElementById("user_imgChange").src="";
+document.getElementById("user_imgChange").style.display="none";
+
+document.getElementById("comp_imgChangerotate").style.display= "none" ;
+document.getElementById("comp_imgChange").src="";
+document.getElementById("comp_imgChange").style.display="none";
+
+
+
+document.getElementById("display_demo").style.display="none"
+document.getElementById("display_userData").style.display="none"
+}
